e461ea2942ae39bc20caee32c73f0a70eb45a436
Samuel Ting <samuelting7@gmail.com>
2025-08-27 16:33:54 +0800

    feat: implemented state management and logic for deleting task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,11 @@
 
 
-export default function TodoItem({ text }) {
+export default function TodoItem({ text, onDeleteTask}) {
+
+	const handleDeleteClick = () => {
+		onDeleteTask(text);
+	}
+
 	return (
 	  <div className="flex items-center justify-between px-3 py-2 rounded-md shadow-sm bg-white my-2">
 		{/* Left: checkbox + text */}
@@ -10,7 +15,10 @@ export default function TodoItem({ text }) {
 		</div>
 
 		{/* Right: delete button */}
-		<button className="text-gray-500 hover:text-red-500">
+		<button
+			className="text-gray-500 hover:text-red-500"
+			onClick={handleDeleteClick}
+		>
 		  🗑
 		</button>
 	  </div>

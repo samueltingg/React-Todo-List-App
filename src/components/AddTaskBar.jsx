bd99a3ecab2bb0979dbe43692fb1798cda1ad56c
Samuel Ting <samuelting7@gmail.com>
2025-08-27 16:12:28 +0800

    feat: added components -> `TodoItem`, `AddTaskbar` feat: implemented useState for handling input in addtaskbar component

---
+++ src/components/AddTaskBar.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function AddTaskBar({ onAddTask }) {
+	const [value, setValue] = useState("");
+	// use 'useState' instead of 'useRef' for live validation
+
+	const handleAddClick = () => {
+		if (value === "") { // check if empty
+			return;
+		}
+		onAddTask(value);
+	}
+
+	return (
+	  <div className="flex w-full bg-gray-100 rounded-full p-1 mt-4">
+		<input
+		  type="text"
+		  placeholder="Add your task"
+			value={value}
+			onChange={ (e) => setValue(e.target.value) } // update state
+		  className="flex-1 px-4 py-2 bg-transparent outline-none text-gray-700"
+		/>
+		<button
+			className="bg-orange-500 text-white px-6 py-2 rounded-full font-semibold"
+			onClick={ handleAddClick }
+			>
+		  ADD +
+		</button>
+	  </div>
+	);
+}
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,19 @@
+
+
+export default function TodoItem({ text }) {
+	return (
+	  <div className="flex items-center justify-between px-3 py-2 rounded-md shadow-sm bg-white my-2">
+		{/* Left: checkbox + text */}
+		<div className="text-black flex items-center gap-2">
+		  <span className="text-orange-500">✔</span>
+		  <p>{text}</p>
+		</div>
+
+		{/* Right: delete button */}
+		<button className="text-gray-500 hover:text-red-500">
+		  🗑
+		</button>
+	  </div>
+
+	);
+  }
